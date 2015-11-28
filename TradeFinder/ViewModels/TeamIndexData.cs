d520a90fe20aba34040721a70e21a22bef167a96
soccercjs2 <soccercjs2@gmail.com>
2015-11-27 19:01:45 -0800

    tracking everything

---
+++ TradeFinder/ViewModels/TeamIndexData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TradeFinder.Data;
+using TradeFinder.Models;
+
+namespace TradeFinder.ViewModels
+{
+    public class TeamIndexData
+    {
+        public League League { get; set; }
+        public List<Team> Teams { get; set; }
+
+        private TradeFinderContext db = new TradeFinderContext();
+
+        public TeamIndexData(int leagudId)
+        {
+            League = db.Leagues.Find(leagudId);
+            Teams = db.Teams.Where(t => t.LeagueId == leagudId).ToList();
+        }
+    }
+}
